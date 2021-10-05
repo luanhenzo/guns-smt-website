f1cc62688f4dcffbccaecd33d2785c622cdd2a4f
Luan Henzo de Almeida <43920813+luansinh0@users.noreply.github.com>
2021-10-05 09:23:05 -0300

    * Fixed Hamburger menu clickable in navigation bar when screen size change, even when it's hidden.

--- guns/static/scripts/hamburger-menu.js
+++ guns/static/scripts/hamburger-menu.js
@@ -2,21 +2,31 @@ const menu = document.querySelector('#nav');
 const menuAnchors = document.querySelectorAll('#nav a');
 let isOpen = false;
 
+function enableMenuAnchors() {
+    for (let i = 0; i < menuAnchors.length; i = i + 1 ) {
+            menuAnchors[i].style.pointerEvents = 'auto'
+    }
+}
+function disableMenuAnchors() {
+    for (let i = 0; i < menuAnchors.length; i = i + 1 ) {
+            menuAnchors[i].style.pointerEvents = 'none'
+    }
+}
+
+window.addEventListener('resize', function() {
+    window.innerWidth > 800 ? enableMenuAnchors() : disableMenuAnchors()
+}, true)
 function changeMenu() {
     let menuHeight = menu.offsetHeight
     console.log(menuHeight)
 
     if (!isOpen) {
         menu.style.top = `${menuHeight}px`;
-        for (let i = 0; i < menuAnchors.length; i = i + 1 ) {
-            menuAnchors[i].style.pointerEvents = 'auto'
-        }
+        enableMenuAnchors()
         isOpen = true;
     } else {
         menu.style.top = '0';
-        for (let i = 0; i < menuAnchors.length; i = i + 1 ) {
-            menuAnchors[i].style.pointerEvents = 'none'
-        }
+        disableMenuAnchors()
         isOpen = false;
     }
 }
